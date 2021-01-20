71e59ebf4b94723235c37d481a2b871fcf7fd9a5
Sandra <alecsandra.serediuc@gmail.com>
2021-01-20 23:36:06 +0100

    basic structure setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import {getFlights} from './Api';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer'
+
+// TO DO
+// 1.DATA AIRPORTS TO FILL THE INPUT ORIGIN/DESTINATION OPTIONS
+// 2.SET STATE FOR THE CHOOSEN AND API CALL FOR THE FLIGHTS/PASSING PARAMS
+// 3.
 
 const App = () => {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+
+  return(
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+      <button onClick={getFlights}>data</button>
+        {/* <button onClick={getAirports}>Air</button>
+        <button onClick={getAirlines}>Airlines</button>
+        <button onClick={getRoute}>Route</button> */}
+    </>
+  )
 }
 
 export default App;
