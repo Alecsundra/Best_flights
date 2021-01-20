f2af5dc56f7a35b2375d52fa26cde2f4b97b359c
Sandra <alecsandra.serediuc@gmail.com>
2021-01-21 00:01:26 +0100

    installed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+// antd css
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
