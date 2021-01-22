65cd1ba5bd4629541483e6b25dfc135ff20798f4
Sandra <alecsandra.serediuc@gmail.com>
2021-01-22 15:27:56 +0100

    getting data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {getFlights} from './Api';
-import Home from './Components/Home';
+import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar';
-import Footer from './Components/Footer'
-
-// TO DO
-// 1.DATA AIRPORTS TO FILL THE INPUT ORIGIN/DESTINATION OPTIONS
-// 2.SET STATE FOR THE CHOOSEN AND API CALL FOR THE FLIGHTS/PASSING PARAMS
-// 3.
+import Footer from './Components/Footer';
+import { Spin } from 'antd';
 
 const App = () => {
+  const [ airports,setAirports ] = useState()
+  const [ airlines,setAirlines ] = useState()
+  const [ mounted, setMounted ] =useState(false)
+
+ useEffect(() => {
+      fetch(`${process.env.REACT_APP_API_URL}/airports/all`, {
+        method: 'GET',
+        headers: new Headers({
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        }),
+    })
+    .then((response) => response.json())
+    .then((data =>setAirports(data)))    .catch(err=>{
+      console.log(err,'error')
+      alert('Found a problem fetching data')
+    })
+      setMounted(true)
+  },[])
+
+
+ useEffect(() => {
+  fetch(`${process.env.REACT_APP_API_URL}/airlines/all`, {
+    method: 'GET',
+    headers: new Headers({
+    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    }),
+ })
+    .then((response) => response.json())
+    // .then((data => console.log(data)))   
+    .then((data =>setAirlines(data)))
+    .catch(err=>{
+      console.log(err,'error')
+      alert('Found a problem fetching data')
+    })
+  },[])
+console.log(airlines)
 
   return(
-    <>
+    <div className = "App">
       <Navbar />
-      <Home />
+      {mounted 
+      ? <Home airports={airports} airlines={airlines}/>
+      : <Spin size="large" style={{marginTop:'2rem'}} /> 
+      }
       <Footer />
-      <button onClick={getFlights}>data</button>
-        {/* <button onClick={getAirports}>Air</button>
-        <button onClick={getAirlines}>Airlines</button>
-        <button onClick={getRoute}>Route</button> */}
-    </>
+    </div>
   )
 }
 
